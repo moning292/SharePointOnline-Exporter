68b3ace7d57eccdd97122c50c49cc174ea714746
Worakorn Chaichakan <moning292@gmail.com>
2018-04-06 00:31:56 +1000

    Add SPOSite and WebDetail classes

---
+++ SPOExporter/SPOSite.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPOExporter
+{
+    //The config file data model that retrieves URL and stored/saved location
+    public class SPOSite
+    {
+        public string URL { get; set; }
+
+        public string savedDir { get; set; } 
+    }
+}
---
+++ SPOExporter/WebDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPOExporter
+{
+    public class WebDetail
+    {
+        //The web data model when retrieving all the sites under the main Sharepoint Online site
+        public string Title { get; set; }
+
+        public string URL { get; set; }
+
+    }
+}
