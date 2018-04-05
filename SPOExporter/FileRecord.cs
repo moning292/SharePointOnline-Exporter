35a76aad81636fb4b04985dfd76a2d73b76f52b2
Worakorn Chaichakan <moning292@gmail.com>
2018-04-06 01:39:54 +1000

    Add FileRecord class

---
+++ SPOExporter/FileRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPOExporter
+{
+    //The file mapping data model in the file
+    public class FileRecord
+    {
+        public string FilePath { get; set; }
+        public string LastModifiedDate { get; set; }
+    }
+}
